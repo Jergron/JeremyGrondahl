a64f6f45a49a6de7edb8902676f8bb4ac2247b3b
Jeremy <jeremygrondahl@gmail.com>
2015-12-11 13:24:02 -0600

    removed narrative photo

--- controllers/MainCtrl.js
+++ controllers/MainCtrl.js
@@ -26,7 +26,7 @@ define([
         url: 'partials/projects.html'
       }
     ];
-
+       
     $(document).ready(function (){
       $('body').css('opacity', '0').fadeTo(1500, 1,'swing'); 
     });
